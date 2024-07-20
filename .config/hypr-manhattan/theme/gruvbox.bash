6add230f3fadc73a75dcb508f736d76a1ecd8136
clevrf0x <favasm72@gmail.com>
2024-07-20 21:18:56 +0530

    Refactor

---
+++ .config/hypr-manhattan/theme/gruvbox.bash
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------
+# Gruvbox Material Dark Color Scheme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#282828'
+foreground='#d4be98'
+
+# Black
+color0='#3c3836'
+color8='#5a524c'
+
+# Red
+color1='#ea6962'
+color9='#ea6962'
+
+# Green
+color2='#a9b665'
+color10='#a9b665'
+
+# Yellow
+color3='#e78a4e'
+color11='#d8a657'
+
+# Blue
+color4='#7daea3'
+color12='#7daea3'
+
+# Magenta
+color5='#d3869b'
+color13='#d3869b'
+
+# Cyan
+color6='#89b482'
+color14='#89b482'
+
+# White
+color7='#d4be98'
+color15='#d4be98'
+
+# Wallpaper (keeping your original setting)
+wallpaper="$HOME/.config/hypr/wallpapers/gruvbox.png"
